f1b4a457fc5094eaa6ceab1aa49c2fb2ad00ef6f
shruti <shruti@startuped.net>
2020-08-15 17:18:21 +0530

    first commit

---
+++ BlazingChat/Server/Controllers/Editor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazingChat.Server.Controllers
+{
+    public class EditorHub: Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "group1");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "group1");
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task BroadcastText(string text)
+        {
+            await Clients.OthersInGroup("group1").SendAsync("ReceiveText", text);
+        }
+    }
+}
